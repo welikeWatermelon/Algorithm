962406edfcd53c29b0ef0cb5fe94e27c8e1e92bc
welikeWatermelon <150949835+welikeWatermelon@users.noreply.github.com>
2024-08-28 17:40:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,17 +1,3 @@
 def solution(numbers):
-    new_numbers1=numbers[:]
-    a=max(new_numbers1)
-    new_numbers1.remove(a)
-    b=max(new_numbers1)
-    sol1=a*b
-    
-    new_numbers2=numbers[:]
-    a=min(new_numbers2)
-    new_numbers2.remove(a)
-    b=min(new_numbers2)
-    sol2=a*b
-    
-    if sol1>sol2:
-        return sol1
-    else:
-        return sol2
+    numbers = sorted(numbers)
+    return max(numbers[0] * numbers[1], numbers[-1]*numbers[-2])
