ca3059e6b71ec39f779206bdf6b182c811829060
김영준 <bill5500@naver.com>
2025-02-12 08:17:17 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
             System.out.println("i = " + i);
+            //이거됨?
         }
     }
 }
